a1de2691330e0c3068480fa178c4d5a61d19a30e
Mark Bell <me@markb.co.uk>
2016-10-15 15:42:08 +0100

    Set up custom bootstrapper

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,32 @@
 using Microsoft.AspNetCore.Builder;
 using Nancy.Owin;
+using Nancy.Configuration;
+using Nancy.Diagnostics;
+using Nancy;
 
 namespace gtdpad
 {
+    public class GTDPadBootstrapper : DefaultNancyBootstrapper
+    {
+        public override void Configure(INancyEnvironment environment)
+        {
+            environment.Diagnostics(
+                enabled: true,
+                password: "test123"
+            );
+
+            environment.Tracing(
+                enabled: false, 
+                displayErrorTraces: true
+            );
+        }
+    }
+
     public class Startup
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.UseOwin(x => x.UseNancy());
+            app.UseOwin(x => x.UseNancy(n => n.Bootstrapper = new GTDPadBootstrapper()));
         }
     }
 }
