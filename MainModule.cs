233ba98665a9a7c634ec825ec21ab6ab5f8e2ea2
Mark Bell <me@markb.co.uk>
2016-10-19 17:54:10 +0100

    Create separate pages module and rename main module

--- MainModule.cs
+++ MainModule.cs
@@ -1,14 +1,12 @@
 using Nancy;
 // using Nancy.Security;
-using Nancy.ModelBinding;
 using Nancy.Authentication.Forms;
-using System;
 
 namespace gtdpad
 {
-    public class Main : NancyModule
+    public class MainModule : NancyModule
     {
-        public Main()
+        public MainModule()
         {
             var db = new Repository();
 
@@ -28,26 +26,6 @@ namespace gtdpad
 
             Get("/logout", args => {
                 return this.LogoutAndRedirect("~/");
-            });
-
-            Post("/pages", args => {
-                return db.CreatePage(this.Bind<Page>());
-            });
-
-            Get("/pages/{id:guid}", args => {
-                return db.ReadPage(args.id);
-            });
-
-            Put("/pages/{id:guid}", args => {
-                return db.UpdatePage(this.Bind<Page>());
-            });
-
-            Delete("/pages/{id:guid}", args => {
-                return db.DeletePage(args.id);
-            });
-
-            Get("/pages", args => {
-                return db.ListPages(new Guid("47D2911F-C127-40C8-A39A-FB13634D2AE9"));
             });
         }
     }
---
+++ PagesModule.cs
@@ -0,0 +1,36 @@
+using Nancy;
+// using Nancy.Security;
+using Nancy.ModelBinding;
+// using Nancy.Authentication.Forms;
+using System;
+
+namespace gtdpad
+{
+    public class PagesModule : NancyModule
+    {
+        public PagesModule() : base("/pages")
+        {
+            var db = new Repository();
+
+            Post("/", args => {
+                return db.CreatePage(this.Bind<Page>());
+            });
+
+            Get("/{id:guid}", args => {
+                return db.ReadPage(args.id);
+            });
+
+            Put("/{id:guid}", args => {
+                return db.UpdatePage(this.Bind<Page>());
+            });
+
+            Delete("/{id:guid}", args => {
+                return db.DeletePage(args.id);
+            });
+
+            Get("", args => {
+                return db.ListPages(new Guid("47D2911F-C127-40C8-A39A-FB13634D2AE9"));
+            });
+        }
+    }
+}
