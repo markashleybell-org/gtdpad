d9802508aab608ff5f43e72bbf503c44844fda28
Mark Bell <me@markb.co.uk>
2016-10-26 08:36:01 +0100

    Scaffolding for front end
    CSS libs and type defs, set up TypeScript config

--- static/js/app.ts
+++ static/js/app.ts
@@ -1,11 +1,14 @@
 /// <reference path="jquery.d.ts" />
 /// <reference path="handlebars.d.ts" />
 /// <reference path="history.d.ts" />
-var HistoryJS = History;
-var GTDPad = (function (window, $, history, tmpl) {
+
+var HistoryJS: Historyjs = <any>History;
+
+var GTDPad = (function(window, $, history, tmpl) {
     function init() {
         console.log('INIT');
     }
+
     return {
         init: init
     };
