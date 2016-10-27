d8802eb5df91bcfb65496f3b83f573c914c5cd67
Mark Bell <me@markb.co.uk>
2016-10-27 07:10:51 +0100

    Render sidebar page list from test data

--- static/js/app.ts
+++ static/js/app.ts
@@ -8,7 +8,9 @@ var GTDPad = (function(window, $, history, tmpl) {
     var _templates = {
             page: null,
             list: null,
-            item: null
+            item: null,
+            sidebarPageList: null,
+            sidebarPage: null
         },
         _ui = {
             content: null,
@@ -36,17 +38,29 @@ var GTDPad = (function(window, $, history, tmpl) {
             ]
         };
 
+        var testPageList = {
+            pages: [
+                { id: 'GUID1', name: 'Test Page 1' },
+                { id: 'GUID2', name: 'Test Page 2' },
+                { id: 'GUID3', name: 'Test Page 3' }
+            ]
+        };
+
         _templates.page = tmpl.compile($('#tmpl-page').html());
         _templates.list = tmpl.compile($('#tmpl-list').html());
         _templates.item = tmpl.compile($('#tmpl-item').html());
+        _templates.sidebarPageList = tmpl.compile($('#tmpl-sidebar-page-list').html());
+        _templates.sidebarPage = tmpl.compile($('#tmpl-sidebar-page').html());
 
         tmpl.registerPartial('list', _templates.list);
         tmpl.registerPartial('item', _templates.item);
+        tmpl.registerPartial('sidebar-page', _templates.sidebarPage);
 
         _ui.content = $('div.content');
         _ui.sidebar = $('div.sidebar');
 
         _ui.content.html(_templates.page(testPage));
+        _ui.sidebar.html(_templates.sidebarPageList(testPageList));
     }
 
     return {
