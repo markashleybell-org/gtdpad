a808e9d2e4167eefefbc9d69e7cb96ec32365b35
Mark Bell <me@markb.co.uk>
2016-10-31 07:00:02 +0000

    Update inspecction SQL

--- db/inspect.sql
+++ db/inspect.sql
@@ -1,5 +1,14 @@
 use gtdpad
 
-select * from pages where deleted is null order by display_order
-select * from lists where deleted is null order by page_id, display_order
-select * from items where deleted is null order by list_id, display_order
+select 'ACTIVE' as Status, * from pages where deleted is null order by display_order
+select 'ACTIVE' as Status, * from lists where deleted is null order by page_id, display_order
+select 'ACTIVE' as Status, * from items where deleted is null order by list_id, display_order
+
+select 'DELETED' as Status, * from pages where deleted is not null order by display_order
+select 'DELETED' as Status, * from lists where deleted is not null order by page_id, display_order
+select 'DELETED' as Status, * from items where deleted is not null order by list_id, display_order
+
+--delete from pages
+--delete from items
+--delete from lists
+
