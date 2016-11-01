6591e9ae19bc0c8f2d07bffb9e9bccf5a76b4cfa
Mark Bell <me@markb.co.uk>
2016-11-01 08:55:27 +0000

    Add USE to kill SQL

--- db/kill.sql
+++ db/kill.sql
@@ -1,4 +1,7 @@
-IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'gtdpad')
+USE master
+GO
+
+IF EXISTS (SELECT * FROM master.dbo.sysdatabases WHERE name = N'gtdpad')
 BEGIN
 	DECLARE @kill varchar(8000) = '';
 	SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), spid) + ';'
