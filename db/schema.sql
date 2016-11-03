ddcd053227eb23ca05c0329fc417dd06a0c9395a
Mark Bell <me@markb.co.uk>
2016-11-03 09:06:20 +0000

    Commit updated schema (w/ordering procs)

--- db/schema.sql
+++ db/schema.sql
@@ -139,3 +139,104 @@ BEGIN
 END	
 GO
 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'GetDisplayOrder') AND type = N'IF')
+DROP FUNCTION GetDisplayOrder
+GO
+
+CREATE FUNCTION GetDisplayOrder (@s NVARCHAR(4000), @sep CHAR(1)) RETURNS TABLE
+AS RETURN (
+	WITH ids(pos, start, stop) AS (
+		SELECT
+			0,
+			1,
+			CHARINDEX(@sep, @s)
+		UNION ALL
+		SELECT
+			pos + 1,
+			stop + 1,
+			CHARINDEX(@sep, @s, stop + 1)
+		FROM
+			ids
+		WHERE
+			stop > 0
+    )
+    SELECT
+		SUBSTRING(@s, start, CASE WHEN stop > 0 THEN stop - start ELSE 4000 END) AS id,
+		pos
+    FROM
+		ids
+)
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'UpdatePageDisplayOrder') AND type in (N'P', N'PC'))
+DROP PROCEDURE UpdatePageDisplayOrder
+GO
+
+CREATE PROCEDURE UpdatePageDisplayOrder (
+	@userid UNIQUEIDENTIFIER,
+	@order NVARCHAR(4000)
+)
+AS
+BEGIN 
+	SELECT id, pos INTO #ids FROM GetDisplayOrder(@order, '|')
+
+	MERGE INTO pages p
+	   USING #ids ids 
+		  ON p.id = ids.id
+		     AND p.user_id = @userid
+	WHEN MATCHED THEN
+	   UPDATE 
+		  SET p.display_order = ids.pos;
+
+	DROP TABLE #ids
+END	
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'UpdateListDisplayOrder') AND type in (N'P', N'PC'))
+DROP PROCEDURE UpdateListDisplayOrder
+GO
+
+CREATE PROCEDURE UpdateListDisplayOrder (
+	@pageid UNIQUEIDENTIFIER,
+	@order NVARCHAR(4000)
+)
+AS
+BEGIN 
+	SELECT id, pos INTO #ids FROM GetDisplayOrder(@order, '|')
+
+	MERGE INTO lists l
+	   USING #ids ids 
+		  ON l.id = ids.id
+		     AND l.page_id = @pageid
+	WHEN MATCHED THEN
+	   UPDATE 
+		  SET l.display_order = ids.pos;
+
+	DROP TABLE #ids
+END	
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'UpdateItemDisplayOrder') AND type in (N'P', N'PC'))
+DROP PROCEDURE UpdateItemDisplayOrder
+GO
+
+CREATE PROCEDURE UpdateItemDisplayOrder (
+	@listid UNIQUEIDENTIFIER,
+	@order NVARCHAR(4000)
+)
+AS
+BEGIN 
+	SELECT id, pos INTO #ids FROM GetDisplayOrder(@order, '|')
+
+	MERGE INTO items i
+	   USING #ids ids 
+		  ON i.id = ids.id
+		     AND i.list_id = @listid
+	WHEN MATCHED THEN
+	   UPDATE 
+		  SET i.display_order = ids.pos;
+
+	DROP TABLE #ids
+END	
+GO
+
