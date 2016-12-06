ae37148eca739d0afbf7c6415f0c3dae5d0c4ca9
Mark Bell <me@markb.co.uk>
2016-12-06 08:52:39 +0000

    Use static constructor to initialise global HttpClient
    The client now mimics the latest version of Chrome, follows redirects
    and correctly handles compressed responses.

--- src/gtdpad/api/infrastructure/Global.cs
+++ src/gtdpad/api/infrastructure/Global.cs
@@ -1,9 +1,24 @@
+using System.Net;
 using System.Net.Http;
 
 namespace gtdpad
 {
     public static class Global
     {
-        public static HttpClient HttpClient = new HttpClient();
+        public static HttpClient HttpClient { get; }
+
+        static Global()
+        {
+            HttpClient  = new HttpClient(new HttpClientHandler {
+                AllowAutoRedirect = true,
+                MaxAutomaticRedirections = 1,
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            });
+
+            HttpClient.DefaultRequestHeaders.Add(
+                "User-Agent", 
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
+            );
+        }
     }
 }
