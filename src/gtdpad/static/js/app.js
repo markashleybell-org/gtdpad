9825541481595a951acc8b51d4d0070a4f4b2916
Mark Bell <me@markb.co.uk>
2016-12-15 14:45:56 +0000

    Refactor metadata retrieval functions
    We assume that 'words' in the item text are separated by one or more
    spaces. This code cleans up a bit more before checking for the existence
    of a URL (and currently only tries to retrieve metadata for a URL if
    it's the first word in the text).

--- src/gtdpad/static/js/app.js
+++ src/gtdpad/static/js/app.js
@@ -93,6 +93,23 @@ var GTDPad = (function (window, console, $, history, tmpl, sortable) {
         input.val('');
         input.val(val);
     }
+    function _isUrl(input) {
+        return input.indexOf('http://') === 0
+            || input.indexOf('https://') === 0;
+    }
+    function _words(input) {
+        return $.trim(input).replace(/\s{2,}/g, ' ').split(' ');
+    }
+    function _fetchUrlMetadata(url, callback) {
+        _xhr('GET', '/metadata', { url: url }, function (data) {
+            if (data) {
+                callback(true, data);
+            }
+            else {
+                callback(false, null);
+            }
+        });
+    }
     function _debounce(func, wait, immediate) {
         var timeout;
         return function () {
@@ -471,11 +488,12 @@ var GTDPad = (function (window, console, $, history, tmpl, sortable) {
         });
     }
     function _onItemFormKeyUp(e) {
-        var text = e.target.value;
-        if (text.indexOf('http') === 0) {
-            _xhr('GET', '/metadata', { url: text }, function (data) {
-                var input = $(e.target).siblings('input[name=title]');
-                if (data) {
+        // TODO: if first word isn't a URL, try the second, third etc. until one is, then break
+        var text = _words(e.target.value)[0];
+        if (_isUrl(text)) {
+            var input = $(e.target).siblings('input[name=title]');
+            _fetchUrlMetadata(text, function (success, data) {
+                if (success) {
                     var title = _htmlDecode(data.title);
                     input.val(title);
                     _ui.currentTitleDisplay.text(title);
