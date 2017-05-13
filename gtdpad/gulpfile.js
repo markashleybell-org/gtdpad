e4f20f41c7c1b31422d24b7d38bfe583ba7be34b
Mark Bell <me@markb.co.uk>
2017-05-13 07:59:02 +0100

    Add gulp tasks for SASS compilation

---
+++ gtdpad/gulpfile.js
@@ -0,0 +1,30 @@
+﻿/// <binding ProjectOpened='watch' />
+'use strict';
+
+// Load gulp and the modules we need
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+// Set some configuration
+var config = {
+    sass: {
+        root: './static/css',
+        sources: './static/css/**/*.scss',
+        options: {
+            precision: 10,
+            outputStyle: 'expanded'
+        }
+    }
+};
+
+// Task to compile Sass
+gulp.task('sass', function () {
+    gulp.src(config.sass.sources)
+        .pipe(sass(config.sass.options).on('error', sass.logError))
+        .pipe(gulp.dest(config.sass.root));
+});
+
+// This task watches all the source files for changes
+gulp.task('watch', function () {
+    gulp.watch(config.sass.sources, ['sass']);
+});
